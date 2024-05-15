bf5f2e3e5d460a41b862b5638dbaff0a9c3ced6b
Karolina Oleśniewicz <olesniewiczkarolina@gmail.com>
2024-05-15 22:44:34 +0200

    added small changes

--- task-management-api/entities/NotificationTraget.cs
+++ task-management-api/entities/NotificationTraget.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int NotificationId { get; set; }
+
         public virtual User User { get; set; }
         public virtual Notification Notification { get; set; }
     }
